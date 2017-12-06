028265810e1c8df292c6cca67853e3f30c58555e
aaronamm <theeranitp@gmail.com>
2017-12-06 08:19:11 +0700

    migration to create and insert provinces, districts and subdistricts data

--- Migrations.cs
+++ Migrations.cs
@@ -7,16 +7,43 @@ using Orchard.ContentManagement.MetaData.Builders;
 using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
 using CodeSanook.AdministrativeDivision.Models;
+using Orchard.Data;
+using Flurl;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using Orchard;
+using Orchard.Environment.Configuration;
+using System.Linq;
+using System.Web.Hosting;
+using System.Reflection;
 
-namespace CodeSanook.AdministrativeDivision {
-    public class Migrations : DataMigrationImpl {
+namespace CodeSanook.AdministrativeDivision
+{
+    public class Migrations : DataMigrationImpl
+    {
+        private readonly ITransactionManager transactionManager;
+        private readonly IOrchardServices orchardService;
+        private readonly IShellSettingsManager shellSettingsManager;
 
-        public int Create() {
+        public Migrations(
+            ITransactionManager transactionManager,
+            IOrchardServices orchardService,
+            IShellSettingsManager shellSettingsManager)
+        {
+            this.transactionManager = transactionManager;
+            this.orchardService = orchardService;
+            this.shellSettingsManager = shellSettingsManager;
+        }
+
+        public int Create()
+        {
 
             SchemaBuilder.CreateTable(typeof(ProvinceRecord).Name, table =>
                 table
                     .Column<int>("Id", column => column.PrimaryKey().Identity())
-                    .Column<string>("Code")
+                    .Column<int>("Code")
                     .Column<string>("NameInThai")
                     .Column<string>("NameInEnglish")
             );
@@ -24,7 +51,7 @@ namespace CodeSanook.AdministrativeDivision {
             SchemaBuilder.CreateTable(typeof(DistrictRecord).Name, table =>
                 table
                     .Column<int>("Id", column => column.PrimaryKey().Identity())
-                    .Column<string>("Code")
+                    .Column<int>("Code")
                     .Column<string>("NameInThai")
                     .Column<string>("NameInEnglish")
                     .Column<int>("ProvinceRecord_Id")
@@ -33,7 +60,7 @@ namespace CodeSanook.AdministrativeDivision {
             SchemaBuilder.CreateTable(typeof(SubdistrictRecord).Name, table =>
                 table
                     .Column<int>("Id", column => column.PrimaryKey().Identity())
-                    .Column<string>("Code")
+                    .Column<int>("Code")
                     .Column<string>("NameInThai")
                     .Column<string>("NameInEnglish")
                     .Column<decimal>("Latitude")
@@ -42,7 +69,74 @@ namespace CodeSanook.AdministrativeDivision {
                     .Column<int>("ZipCode")
             );
 
+            //TODO add indexes and foreign keys
+
             return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            //insert administrative division data
+            ServerConnection connectionContext = null;
+            try
+            {
+                var connectionString = GetConnectionString();
+                var connection = new SqlConnection(connectionString);
+                //var session = transactionManager.GetSession();
+                //var connection = (SqlConnection)session.Connection;
+
+
+                var server = new Server(new ServerConnection(connection));
+                var script = GetAdminstrativeDivisionDataScript();
+
+                connectionContext = server.ConnectionContext;
+                //connectionContext.StatementTimeout = 60;
+                connectionContext.BeginTransaction();
+                connectionContext.ExecuteNonQuery(script);
+                connectionContext.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                //roll back if insert fail
+                if (connectionContext != null && connectionContext.IsOpen)
+                {
+                    connectionContext.RollBackTransaction();
+                }
+                //throw exception to handle by default handler  
+                throw;
+            }
+
+            return 2;
+        }
+
+        private string GetConnectionString()
+        {
+            var settings = shellSettingsManager.LoadSettings();
+            var defaultSetting = settings.Where(setting => setting.Name == "Default").Single(); ;
+            var connectionString = defaultSetting.DataConnectionString;
+            return connectionString;
+        }
+
+        private string GetAdminstrativeDivisionDataScript()
+        {
+            var scriptUrl = Url.Combine(
+                "~/Modules",
+                this.GetType().Assembly.GetName().Name,
+                "Contents",
+                "AdministrativeDivisionData.sql");
+            var httpContext = orchardService.WorkContext.HttpContext;
+
+            var scriptPath = //httpContext.Request.MapPath(scriptUrl);
+            HostingEnvironment.MapPath(scriptUrl);
+            return File.ReadAllText(scriptPath);
+        }
+
+        private string GetRootPath()
+        {
+            var codeBaseUrl = Assembly.GetExecutingAssembly().CodeBase;
+            var filePathToCodeBase = new Uri(codeBaseUrl).LocalPath;
+            var directoryPath = Path.GetDirectoryName(filePathToCodeBase);
+            return directoryPath;
         }
     }
 }
