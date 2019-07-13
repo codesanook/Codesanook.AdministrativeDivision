7f9d548ec5254caf7cbf95d3a94670a42ce6dc71
aaronamm <theeranitp@gmail.com>
2019-07-13 12:35:56 +0700

    Update AssemblyTitle

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSanook.AdministrativeDivision")]
+[assembly: AssemblyTitle("Codesanook.AdministrativeDivision")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Orchard")]
